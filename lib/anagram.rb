17573361af2a658bef45b2b74adbde5a506e9b06
luis leon <leonluis380@yahoo.com>
2021-06-16 13:06:35 -0400

    commit tree 2a4ad2ac3186ec1359491c5384ef6725c3d4dcb0

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+class Anagram
+attr_accessor :word
+    
+
+    def initialize(letters)
+        @word = letters
+    end
+
+    def match(dictionary)
+        anagram = []
+        dictionary.select do |word|
+          @word.split("").sort == word.split("").sort   
+            
+         
+        end
+       
+     end
+
+
+
+
+end
